6b3522813c2f90b7f824c5aff0acb6b2a0d19150
savanto <savanto@users.noreply.github.com>
2015-08-21 13:05:34 -0700

    Add some more stuff to class Decisions

--- app/src/main/java/com/savanto/jumptodecisions/Decisions.java
+++ app/src/main/java/com/savanto/jumptodecisions/Decisions.java
@@ -1,5 +1,12 @@
 package com.savanto.jumptodecisions;
 
-public final class Decisions {
+public class Decisions {
+    private static final String WORD_1 = "octopus";
+    private static final String WORD_2 = "watermelon";
 
+    private String word;
+
+    public Decisions() {
+        this.word = WORD_1;
+    }
 }
